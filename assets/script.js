c5b899d22255403e543d89c89417fbe49f8f1308
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-20 13:39:35 -0500

    added js comments based on what I want the code to do

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const apiKey = "8ad839b65264bae52bc1d8df55faaf48";
+const apiBaseUrl: "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const form = document.getElementById("city-form");
+const cityInput = document.getElementById("city-input");
+// add more for the current weather block, 5 day forecast, and search history once I fill those in on html //
+
+
+// add eventlistener for the city submitted into the form
+
+// add event listener for the search history ??
+
+// create function for fetching a city's coordinates from the api
+
+// create function for fetching the weather data
+
+// display the forecast
+
+// add that city to the search history
+
+// search history is stored and loaded when the app is loaded
