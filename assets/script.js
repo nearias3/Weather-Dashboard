a8cb4d1312c3457ef7a3aca88a7ce1fc92508ea5
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-20 14:05:53 -0500

    added code for submission form, still not working

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,25 @@ const apiKey = "8ad839b65264bae52bc1d8df55faaf48";
 const apiBaseUrl: "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 const form = document.getElementById("city-form");
 const cityInput = document.getElementById("city-input");
-// add more for the current weather block, 5 day forecast, and search history once I fill those in on html //
-
+const currentWeather = document.getElementById("current-weather");
+const forecast = document.getElementById("forecast");
+const searchHistory = document.getElementById("search-history");
 
 // add eventlistener for the city submitted into the form
+form.addEventListener("submit", (event)) => {
+    event.preventDefault();
+    const city = cityInput.value.trim(); //asking for trimmed value from the input box
+    if (city) {
+        getCityCoordinates(city); // fetch the city coordinates
+        addToSearchHistory(city); //need code to add the search to the search history
+        cityInput.value = ""; // clearing the input value after hitting submit
+    }
+}
+
 
 // add event listener for the search history ??
+
+// what if a city with no values is given
 
 // create function for fetching a city's coordinates from the api
 
